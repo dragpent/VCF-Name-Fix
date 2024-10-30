ff84c018fd52b2d9468f9a525d5be040c0829653
Dragpent <dragpent@gmail.com>
2024-10-29 20:50:50 -0400

    Add project files.

---
+++ VCF Name Fix/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace VCF_Name_Fix
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnProcess = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            btnOpenFile = new Button();
+            lblMessages = new Label();
+            SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            btnProcess.Enabled = false;
+            btnProcess.Location = new Point(199, 12);
+            btnProcess.Name = "btnProcess";
+            btnProcess.Size = new Size(147, 49);
+            btnProcess.TabIndex = 0;
+            btnProcess.Text = "Process and Save";
+            btnProcess.UseVisualStyleBackColor = true;
+            btnProcess.Click += btnProcess_Click;
+            // 
+            // btnOpenFile
+            // 
+            btnOpenFile.Location = new Point(12, 12);
+            btnOpenFile.Name = "btnOpenFile";
+            btnOpenFile.Size = new Size(147, 49);
+            btnOpenFile.TabIndex = 1;
+            btnOpenFile.Text = "Open VCF File";
+            btnOpenFile.UseVisualStyleBackColor = true;
+            btnOpenFile.Click += btnOpenFile_Click;
+            // 
+            // lblMessages
+            // 
+            lblMessages.Location = new Point(12, 80);
+            lblMessages.Name = "lblMessages";
+            lblMessages.Size = new Size(334, 53);
+            lblMessages.TabIndex = 2;
+            lblMessages.Text = "MAKE SURE TO HAVE BACKUP OF ORGINAL VCF\r\n\r\nClick on Open VCF File and select your VCF file";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(358, 142);
+            Controls.Add(lblMessages);
+            Controls.Add(btnOpenFile);
+            Controls.Add(btnProcess);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "VCF Name Fix";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnProcess;
+        private OpenFileDialog openFileDialog1;
+        private Button btnOpenFile;
+        private Label lblMessages;
+    }
+}
---
+++ VCF Name Fix/Form1.cs
@@ -0,0 +1,138 @@
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace VCF_Name_Fix
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            // Filter only on VCF file
+
+            openFileDialog1.Filter = "VCF files|*.vcf";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                // Clear messages
+                lblMessages.Text = "";
+                lblMessages.Visible = false;
+
+                var fileContent = string.Empty;
+                var filePath = string.Empty;
+
+                //Get the path of specified file
+                filePath = openFileDialog1.FileName;
+
+                lblMessages.Text = filePath + " loaded... \r\n\r\n Please click Process to create new VCF file with fixes...";
+                lblMessages.Visible = true;
+
+                btnProcess.Enabled = true;
+            }
+            else
+            {
+                // Show error message?
+                lblMessages.Text = "Please select VCF file only.";
+                lblMessages.Visible = true;
+
+                btnProcess.Enabled = false;
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+
+            //Read the contents of the file into a stream
+            var fileStream = openFileDialog1.OpenFile();
+
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                fileContent = reader.ReadToEnd();
+
+                string[] stringSeparators = new string[] { "\r\n" };
+                string[] vcfData = fileContent.Split(stringSeparators, StringSplitOptions.None);
+
+                var currentName = "";
+                var fullName = "";
+                var fixedName = "";
+
+                // Now we need to loop through the results
+                for (int i = 0; i < vcfData.Length; i++)
+                {
+                    var vcfRow = vcfData[i];
+
+                    // Let's check what data we are on
+                    if (vcfRow.StartsWith("FN:"))
+                    {
+                        // We are on the first name
+                        currentName = vcfRow.TextAfter("FN:");
+                    }
+                    else if (vcfRow.StartsWith("N:"))
+                    {
+                        // We are on the full name
+                        fullName = vcfRow.TextAfter("N:");
+
+                        // With this full name let's split out what it 'should be'
+                        if (fullName.Contains(";"))
+                        {
+                            string[] fullNameParts = fullName.Split(';');
+
+                            // Check we have the right data
+                            // 0 Family Name
+                            // 1 Given Name
+                            // 2 Middle Name
+                            // 3 Prefixes
+                            // 4 Suffixes
+                            if (fullNameParts.Length == 5)
+                            {
+                                // Let's build out the 'correct' name
+                                fullNameParts.Switch(3, 0);
+                                fixedName = String.Join(" ", fullNameParts.Where(s => !string.IsNullOrEmpty(s)));
+
+                                // Now that we have the 'correct' name let's compare
+                                if (!fixedName.Equals(currentName))
+                                {
+                                    // This means the name is WRONG let's update
+                                    // FN appears to always be before N
+                                    vcfData[i - 1] = "FN:" + fixedName;
+                                }
+                            }
+                        }
+                        // Here let's reset our data
+
+                        currentName = "";
+                        fullName = "";
+                        fixedName = "";
+                    }
+                }
+
+                // Now that we are here we have processed everything
+                // Let's save to desktop
+                string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "FixedVCF.vcf");
+                File.WriteAllLines(filePath, vcfData);
+
+                lblMessages.Text = "Fixed file saved to: " + filePath;
+            }
+        }
+    }
+    public static class Extension
+    {
+        public static string TextAfter(this string value, string search)
+        {
+            return value.Substring(value.IndexOf(search) + search.Length);
+        }
+        public static void Switch<T>(this IList<T> array, int index1, int index2)
+        {
+            var aux = array[index1];
+            array[index1] = array[index2];
+            array[index2] = aux;
+        }
+    }
+}
